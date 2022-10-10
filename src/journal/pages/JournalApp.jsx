d7c7036a675fb5d47fb256cbf3916a475a6f0849
Louis <poostmaan@gmail.com>
2022-10-10 19:10:39 -0400

    JournalApp modified

--- src/journal/pages/JournalApp.jsx
+++ src/journal/pages/JournalApp.jsx
@@ -1,11 +1,13 @@
 import { MailOutlined } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalApp = () => {
   return (
-    <>
-    <Typography variant="h2">JournalApp</Typography>
-    <MailOutlined />
-    </>
+    <JournalLayout>
+      <Typography variant="h2">JournalApp</Typography>
+      <Typography variant="pre">Dolore est anim ea eu deserunt ad sint enim ut. Sint deserunt qui amet enim ad quis anim officia. Consectetur dolore commodo consectetur ex incididunt culpa tempor laborum ea mollit deserunt deserunt. Quis consequat pariatur aliquip ipsum pariatur incididunt officia. Eiusmod non anim qui laboris nostrud minim.</Typography>
+      <MailOutlined />
+    </JournalLayout>
   )
 }
